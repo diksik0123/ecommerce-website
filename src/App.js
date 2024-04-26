1afebd2bb5e8d71093d6b0e32dd387be50a7bcce
Paul Bocalan <113324083+diksik0123@users.noreply.github.com>
2024-04-26 11:40:49 +0800

    import modal component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ProductList from './components/ProductList';
 import Details from './components/Details';
 import Cart from './components/Cart';
 import Default from './components/Default';
+import Modal from './components/Modal';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/cart" element={<Cart />} />
         <Route path="*" element={<Default />} />
       </Routes>
+      <Modal />
     </React.Fragment>
   );
 }
