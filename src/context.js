a0be2ebf4243612d39ccf4f841700d368e78a6fd
Paul Bocalan <113324083+diksik0123@users.noreply.github.com>
2024-04-26 09:57:54 +0800

    componentDidMount

--- src/context.js
+++ src/context.js
@@ -7,14 +7,29 @@ const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
     state = {
-        products: storeProducts,
+        products: [],
         detailProduct: detailProduct
     }
+    componentDidMount() {
+      this.setProducts();
+    }
+
+    
     handleDetail = () => {
         console.log('hello from detail');
     }
-    addToCart = () => {
-        console.log('hello from add to cart');
+    addToCart = (id) => {
+        console.log(`hello from add to cart. id is ${id}`);
+    }
+    setProducts = () => {
+        let tempProducts = [];
+        storeProducts.forEach(item => {
+            const singleItem = { ...item };
+            tempProducts = [...tempProducts, singleItem];
+        });
+        this.setState(() => {
+            return { products:tempProducts }
+        })
     }
 
   render() {
