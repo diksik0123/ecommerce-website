3d0cc4646a9da4803b1094a03161cbfda910abfd
Paul Bocalan <113324083+diksik0123@users.noreply.github.com>
2024-04-26 12:01:25 +0800

    add method for cart

--- src/context.js
+++ src/context.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { storeProducts, detailProduct } from './data';
+import { clear } from '@testing-library/user-event/dist/clear';
 
 const ProductContext = React.createContext();
 //Provider
@@ -12,6 +13,9 @@ class ProductProvider extends Component {
         cart: [],
         modalOpen: false,
         modalProduct: detailProduct,
+        cartSubTotal: 0,
+        cartTax: 0,
+        cartTotal: 0
     }
 
     setProducts = () => {
@@ -65,7 +69,22 @@ class ProductProvider extends Component {
         return {modalOpen:false}
       })
     }
-  
+
+    increment = id => {
+      console.log('this is increment method');
+    }
+    
+    decrement = id => {
+      console.log('this is decrement method');
+    }
+
+    removeItem = id => {
+      console.log('item removed');
+    }
+
+    clearCart = () => {
+      console.log('cart was cleared');
+    }
 
   render() {
     return (
@@ -74,7 +93,11 @@ class ProductProvider extends Component {
         handleDetail: this.handleDetail,
         addToCart: this.addToCart,
         openModal: this.openModal,
-        closeModal: this.closeModal
+        closeModal: this.closeModal,
+        increment: this.increment,
+        decrement: this.decrement,
+        removeItem: this.removeItem,
+        clearCart: this.clearCart
       }}>
         {this.props.children}
       </ProductContext.Provider>
