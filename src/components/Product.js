686060142d46e09a4abdd5792c0dee84f78f4c12
Paul Bocalan <113324083+diksik0123@users.noreply.github.com>
2024-04-26 11:42:55 +0800

    add modal method in add to cart button

--- src/components/Product.js
+++ src/components/Product.js
@@ -10,8 +10,11 @@ export default class Product extends Component {
     return (
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
         <div className='card'>
-          <div className="img-container p-5" 
-          onClick={()=>console.log('you haved clicked me')}>
+          <ProductConsumer>
+          {value => (<div className="img-container p-5" 
+          onClick={()=>
+          value.handleDetail(id)
+          }>
           <Link to="/details">
           <img src={img} 
           alt="product" 
@@ -19,8 +22,11 @@ export default class Product extends Component {
           </Link>
           <button className='cart-btn' 
           disabled={inCart ? true : false} 
-          onClick={()=>
-            {console.log('added to the cart')}}>
+          onClick={()=>{
+            value.addToCart(id);
+            value.openModal(id);
+          }
+            }>
             {inCart ? 
             (<p className='text-capitalize mb-0' disabled>
               {" "}
@@ -28,7 +34,8 @@ export default class Product extends Component {
               </p>) : 
               (<i className='fas fa-cart-plus'/>)}
           </button>
-          </div>
+          </div>)}
+          </ProductConsumer>
 
           {/* card footer */}
           <div className='card-footer d-flex justify-content-between'>
