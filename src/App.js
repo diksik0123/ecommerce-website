a022fdef5e10cac548c5031450c66933ab9e6da7
Paul Bocalan <113324083+diksik0123@users.noreply.github.com>
2024-05-03 10:58:27 +0800

    create homepage component

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ import Details from './components/Details';
 import Cart from './components/Cart';
 import Default from './components/Default';
 import Modal from './components/Modal';
+import Homepage from './components/Homepage';
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<ProductList />} />
+        <Route exact path="/" element={<Homepage />} />
+        <Route path="/products" element={<ProductList />} />
         <Route path="/details" element={<Details />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="*" element={<Default />} />
