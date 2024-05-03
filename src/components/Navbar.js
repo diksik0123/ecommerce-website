be395f33162a9d3ba0036178f510033549f66356
Paul Bocalan <113324083+diksik0123@users.noreply.github.com>
2024-05-03 11:12:07 +0800

    chnage color of brand

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,13 +8,9 @@ export default class navbar extends Component {
   render() {
     return <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
 
-      {/* 
-  https://www.iconfinder.com/icons/1243689/call_phone_icon
-  Creative Commons (Attribution 3.0 Unported);
-  https://www.iconfinder.com/Makoto_msk */}
   <Link to="/" className='text-decoration-none fs-3'>
     <img src={logo} alt="store" className="navbar-brand mb-1"/>
-    <span>Navbar</span>
+    <span  style={{color:'var(--mainWhite)'}}>Navbar</span>
     </Link>
     
     <div className='ms-auto my-2 d-flex justify-content-end gap-4'>
